0c4f6a2080f163fffdbf1b6cade999df54709a31
暮歌丶 <329167139@qq.com>
2022-04-02 19:51:45 +0800

    add

---
+++ 2000 ~ 2099/_2000_Reverse_Prefix_of_Word/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {}
+
+struct Solution;
+
+impl Solution {
+    pub fn reverse_prefix(mut word: String, ch: char) -> String {
+        if let Some(end) = word.find(ch) {
+			// 1. std reverse
+            unsafe {
+                word.as_bytes_mut().get_unchecked_mut(..=end).reverse();
+            }
+			// 2. myself reverse
+            // Self::swap(unsafe { word.as_bytes_mut().get_unchecked_mut(..=end) });
+        }
+
+        word
+    }
+
+    fn swap(str: &mut [u8]) {
+        if str.is_empty() {
+            return;
+        }
+        let (mut l, mut r) = (0, str.len() - 1);
+
+        while l < r {
+            unsafe {
+                std::ptr::swap(str.get_unchecked_mut(l), str.get_unchecked_mut(r));
+            }
+            l += 1;
+            r -= 1;
+        }
+    }
+}
